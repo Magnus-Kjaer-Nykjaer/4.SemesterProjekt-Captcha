093f8dc90b90b2d4af69c85b90244e68bbfb5971
MknMan <mkn@unik.dk>
2022-05-04 15:32:21 +0200

    Label side samt client med login

---
+++ CaptchaSolution/Captcha.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Captcha.MVC.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
---
+++ CaptchaSolution/Captcha.MVC/Models/UploadFileModel .cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Threading.Tasks;
+namespace Captcha.MVC.Models
+{
+  public class UploadFileModel : PageModel
+  {
+    private IWebHostEnvironment _environment;
+    public UploadFileModel(IWebHostEnvironment environment)
+    {
+      _environment = environment;
+    }
+    [BindProperty]
+    public IFormFile Upload { get; set; }
+    public async Task OnPostAsync()
+    {
+      var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
+      using (var fileStream = new FileStream(file, FileMode.Create))
+      {
+        await Upload.CopyToAsync(fileStream);
+      }
+    }
+  }
+}
