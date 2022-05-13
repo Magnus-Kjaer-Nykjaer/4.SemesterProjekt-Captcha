739e15f5fa30f4cf6de8b98c8688d4afc805e2cf
Magnus-Kjaer-Nykjaer <mkn@eoz.dk>
2022-05-13 10:45:29 +0200

    Næsten færdig adminpanel

--- CaptchaSolution/Captcha.MVC/Controllers/AdminController.cs
+++ CaptchaSolution/Captcha.MVC/Controllers/AdminController.cs
@@ -1,23 +1,42 @@
-﻿using Captcha.MVC.Models;
+﻿using System;
+using System.Threading.Tasks;
+using Captcha.MVC.Service;
 using Captcha.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Captcha.MVC.Controllers
 {
   [Authorize(Roles = "Admin")]
   public class AdminController : Controller
   {
-    public IActionResult Index()
-    {
-      return View();
-    } 
+    private readonly ILogger<AdminController> _logger;
+    private readonly ICaptchaService _captchaService;
 
-    public IActionResult CreateLabel(CaptchaLabelDto label)
+    public AdminController(ICaptchaService captchaService, ILogger<AdminController> logger)
     {
-
-      return View();
+      _captchaService = captchaService;
+      _logger = logger;
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index(CaptchaLabelDto label)
+    {
+      try
+      {
+        if (ModelState.IsValid)
+        {
+          await _captchaService.PostCaptcha(label);
+          return RedirectToAction(nameof(Index));
+        }
+      }
+      catch
+      {
+        _logger.LogError("Der gik et eller andet galt under oprettelsen");
+      }
+      return RedirectToAction(nameof(Index));
+    }
   }
 }
